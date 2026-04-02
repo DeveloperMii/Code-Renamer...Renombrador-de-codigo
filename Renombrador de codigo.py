303f8edaa53c17ac3cea100efbd078355a7bef8e
DevMi! <condevmiitac@outlook.com>
2026-04-02 15:55:50 -0500

    Correccion en el nombre de las variables

--- Renombrador de codigo.py
+++ Renombrador de codigo.py
@@ -2,37 +2,39 @@
 from pathlib import Path
 
 #Variables
-Ruta : str
-Opcion : bool = False
+Route : str
+Option : bool = False
 CDigitS : str
 CDigitI : int = 1
-Archivos : list = []
-DesdeS : str = "000"
-DesdeI : int = None
-HastaS : str = "000"
-HastaI : int = None
-CuantoS : str = "000"
-CuantoI : int = None
-Renombrable : bool = True
+Files : list = []
+FromS : str = "000"
+FromI : int = None
+UntillS : str = "000"
+UntillI : int = None
+How_manyS : str = "000"
+How_manyI : int = None
+Renameable : bool = True
 
 #Validacion de ruta
 while True:
-    Ruta = input("En que ruta carpeta estan los archivos:  ")
-    if Path(Ruta).exists() and Ruta != "" and Ruta != ".":
-        print("La ruta escogida es: " + Ruta)
+    Route = input("En que ruta carpeta estan los archivos \n Si es la misma en la que esta este script pulse enter \n :  ")
+    if Route != "" and Route != ".":
+        Route = __file__
+    if Path(Route).exists():
+        print("La ruta escogida es: " + Route)
         while True:
-            match input("Si no esta seguro presione 1 en caso de estar seguro presione 2: "):
+            match input("Si no esta seguro presione 1 \n En caso de estar seguro presione 2 \n : "):
                 case "1":
-                    Opcion = False
+                    Option = False
                     break
                 case "2":
-                    Opcion = True
+                    Option = True
                     break
                 case _:
                     print("Ingrese un valor valido")   
     else:
         print("La ruta es invalida")
-    if Opcion:
+    if Option:
         break
 
 #Cantidad de digitos del codigo
@@ -45,66 +47,66 @@ while True:
         print("Valor invalido \nIngrese uno nuevo")
 
 #Empaquetado de archivos
-for i in Path(Ruta).iterdir():
-    Archivos.append(i)
+for i in Path(Route).iterdir():
+    Files.append(i)
     for j in range(0,CDigitI):
         if len(i.stem) > j:
             if not i.stem[j].isdigit():
-                Archivos.remove(i)
+                Files.remove(i)
                 break
 
 #Desde donde, Hasta donde y en cuanto se espera cambiar
-while DesdeI == None and HastaI == None and CuantoI == None:
+while FromI == None and UntillI == None and How_manyI == None:
     while True:
-        while DesdeI == None:
-            DesdeS = input("Ingrese desde que numero quiere iniciar: ")
-            if DesdeS.isdigit:
-                DesdeI = int(DesdeS)
+        while FromI == None:
+            FromS = input("Ingrese desde que numero quiere iniciar: ")
+            if FromS.isdigit:
+                FromI = int(FromS)
             else:
                 print("Valor invalido \nIngrese uno nuevo")
-        while HastaI == None:
-            HastaS = input("Ingrese hasta que numero quiere llegar: ")
-            if HastaS.isdigit():
-                HastaI = int(HastaS)
+        while UntillI == None:
+            UntillS = input("Ingrese hasta que numero quiere llegar: ")
+            if UntillS.isdigit():
+                UntillI = int(UntillS)
             else:
                 print("Valor invalido \nIngrese uno nuevo")
-        if DesdeI < HastaI:
+        if FromI < UntillI:
             break
         else:
-            DesdeI = None
-            HastaI = None
+            FromI = None
+            UntillI = None
             print("El valor hasta donde va a llegar no puede ser mayor que desde donde empieza")
-    while CuantoI == None:
-        CuantoS = input("¿En cuanto lo quiere aumentar o restar?: ")
-        if CuantoS[0] == "-":
-            if CuantoS[1:].isdigit():
-                CuantoI = int(CuantoS[1:]) * -1
+    while How_manyI == None:
+        How_manyS = input("¿En cuanto lo quiere aumentar o restar?: ")
+        if How_manyS[0] == "-":
+            if How_manyS[1:].isdigit():
+                How_manyI = int(How_manyS[1:]) * -1
         else:
-            if CuantoS.isdigit():
-                CuantoI = int(CuantoS)
+            if How_manyS.isdigit():
+                How_manyI = int(How_manyS)
             else:
                 print("Valor invalido \nIngrese uno nuevo")
 
 #Funcion de renombrar
 def Renombrador(i : int):
-    Renombrable = True
-    if int(Archivos[i].stem[:CDigitI]) >= DesdeI and int(Archivos[i].stem[:CDigitI]) <= HastaI:
-        for j in Archivos:
-            if int(Path(j).stem[:CDigitI]) == int(Archivos[i].stem[:CDigitI]) + CuantoI:
-                Renombrable = False
-                print(str(Archivos[i].stem) + " se omitio porque " + str(j.stem) + " Existe")
+    Renameable = True
+    if int(Files[i].stem[:CDigitI]) >= FromI and int(Files[i].stem[:CDigitI]) <= UntillI:
+        for j in Files:
+            if int(Path(j).stem[:CDigitI]) == int(Files[i].stem[:CDigitI]) + How_manyI:
+                Renameable = False
+                print(str(Files[i].stem) + " se omitio porque " + str(j.stem) + " Existe")
                 break
-        if Renombrable and Path(Archivos[i]).exists():
-            print(str(Archivos[i].stem) + " -> " + str(int(Archivos[i].stem[:CDigitI]) + CuantoI).zfill(CDigitI) + str(Archivos[i].name[CDigitI:]))
-            Path.rename(Archivos[i],str(Archivos[i].parent) + "/" + str(int(Archivos[i].stem[:CDigitI]) + CuantoI).zfill(CDigitI) + str(Archivos[i].name[CDigitI:]))
-            Archivos[i] = Path("".zfill(CDigitI)).name
+        if Renameable and Path(Files[i]).exists():
+            print(str(Files[i].stem) + " -> " + str(int(Files[i].stem[:CDigitI]) + How_manyI).zfill(CDigitI) + str(Files[i].name[CDigitI:]))
+            Path.rename(Files[i],str(Files[i].parent) + "/" + str(int(Files[i].stem[:CDigitI]) + How_manyI).zfill(CDigitI) + str(Files[i].name[CDigitI:]))
+            Files[i] = Path("".zfill(CDigitI)).name
 
 #Variacion del renombrador segun la cantidad de aumento
-if CuantoI < 0:
-    for i in range(0, len(Archivos)):
+if How_manyI < 0:
+    for i in range(0, len(Files)):
         Renombrador(i)
-elif CuantoI > 0:
-    for i in range(len(Archivos) - 1, -1, -1):
+elif How_manyI > 0:
+    for i in range(len(Files) - 1, -1, -1):
         Renombrador(i)
 else:
     print("No se hizo nada")
